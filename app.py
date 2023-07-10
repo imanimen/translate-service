3cec114521304ad145de725894e06bfeea5a7d63
iman <imanimen81@gmail.com>
2023-07-10 09:40:30 +0330

    FIX CORS error in the app.py

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ from pydantic import BaseModel
 from typing import Optional
 import re
 from deep_translator import GoogleTranslator
+from fastapi.middleware.cors import CORSMiddleware
 
-# Define the FastAPI app
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define the request payload schema
 class TranslationRequest(BaseModel):
